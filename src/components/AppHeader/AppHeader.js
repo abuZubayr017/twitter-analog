48f69efad7d6301f1d454045f17664fe7b362116
odilov sarvar <odilovsarvar@odilovui-MacBookPro.local>
2022-10-05 15:21:37 +0900

    starting

---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,12 @@
+import "./AppHeader.css"
+
+const AppHeader = ({liked, allPosts}) => {
+    return(
+        <div className="app-header d-flex">
+            <h1>Sarvar Odilov</h1>
+            <h2>{allPosts} post, like {liked}</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
